56a1169946c23f3e2bd43ea0689842769f000e92
Markus <markust48@gmail.com>
2023-05-10 09:55:15 +0300

    Add possibility to create new event

--- website/main.py
+++ website/main.py
@@ -1,10 +1,48 @@
-from flask import Blueprint, render_template
+from datetime import datetime
+
+from flask import Blueprint, redirect, render_template, request, url_for
 from flask_login import login_required, current_user
+
+from .models import Event
+from website.static.db import db
 
 
 main = Blueprint('main', __name__)
 
-@main.route('/', methods=['GET', 'POST'])
+
+@main.route('/')
 @login_required
 def home():
-    return render_template("home.html")
+    events = Event.query.all()
+    return render_template("home.html", events=events)
+
+
+@main.route('/create_event')
+@login_required
+def create_event():
+    events = Event.query.all()
+    return render_template("create_event.html", events=events)
+
+
+@main.route('/create_event', methods=["POST"])
+@login_required
+def create_event_post():
+    name = request.form.get('name')
+    description = request.form.get('description')
+    category = request.form.get('category')
+    date = datetime.now()
+    max_participants = request.form.get('max_participants')
+    location = request.form.get('location')
+
+    new_event = Event(user_id=current_user.id,
+                      name=name,
+                      description=description,
+                      category=category,
+                      date=date,
+                      max_participants=max_participants,
+                      location=location)
+    # Adding the event to the database
+    db.session.add(new_event)
+    db.session.commit()
+
+    return redirect(url_for("main.home"))
--- website/models.py
+++ website/models.py
@@ -1,9 +1,22 @@
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 from website.static.db import db
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String(150))
+    description = db.Column(db.String(1000))
+    category = db.Column(db.String(100))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    max_participants = db.Column(db.Integer)
+    location = db.Column(db.String(200))
 
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
+    events = db.relationship('Event')
